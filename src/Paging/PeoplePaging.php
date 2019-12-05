3b11c2831ed0d4bd69fea4e7959370d3b968c7f5
Christophe Guinot <christophe.guinot@hotmail.fr>
2019-12-05 12:18:12 +0100

    change paging for add userClient creteria

--- src/Paging/PeoplePaging.php
+++ src/Paging/PeoplePaging.php
@@ -4,36 +4,41 @@ namespace App\Paging;
 
 use App\Exceptions\ApiException;
 use App\Repository\PersonRepository;
+use Symfony\Component\Security\Core\Security;
 
 class PeoplePaging
 {
-    private const NB_PEOPLE_PAGED = 5;
-
+    private const NB_PEOPLE_PAGED = 2;
     private $repository;
+    private $security;
     private $nbPeople;
     private $maxPages;
+    private $idUserClient;
 
     public function __construct(
-        PersonRepository $repository
+        PersonRepository $repository,
+        Security $security
     )
     {
         $this->repository = $repository;
-        $this->nbpeople = $this->repository->count([]);
-        $this->maxPages = intval($this->nbpeople / self::NB_PEOPLE_PAGED);
+        $this->security = $security;
+        $this->idUserClient = $this->security->getUser()->getId();
+        $this->nbpeople = $this->repository->count(['userClient'=>$this->idUserClient]);
+        $this->maxPages = intval(ceil($this->nbpeople / self::NB_PEOPLE_PAGED));
     }
 
     public function getDatas($page){
 
         if($page === null){
-            return $people = $this->repository->findAll();
+            return $people = $this->repository->findBy(['userClient'=>$this->idUserClient], ['email'=>'ASC']);
         }
 
         if(1 > $page || $page > $this->maxPages){
             throw new ApiException('The page must be between 1 and '.$this->maxPages.'.', 404);
         }
 
-        $offset = $page * self::NB_PEOPLE_PAGED;
-        return $people = $this->repository->findBy([], [], self::NB_PEOPLE_PAGED, $offset);        
+        $offset = self::NB_PEOPLE_PAGED*($page -1);
+        return $people = $this->repository->findBy(['userClient'=>$this->idUserClient], ['email'=>'ASC'], self::NB_PEOPLE_PAGED, $offset);        
     }
     
 }
